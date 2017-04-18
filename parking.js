d869c2db910bc38f193286b3b006bb167dbc10fe
Alex Barstow <alexander.barstow@gmail.com>
2017-04-18 19:55:35 -0400

    refactor wip; added households object, sendReminders now takes two arguments

--- parking.js
+++ parking.js
@@ -1,13 +1,67 @@
+'use strict'
+
 const dotenv = require('dotenv').config();
 const twilio = require('twilio');
 const client = twilio(process.env.accountSID, process.env.authToken);
 
-// 1st and 3rd Tuesdays - Park on even side
-// 2nd and 4th Wednesdays - Park on odd side
 
-const phoneNumbers = [
-  // phone numbers formatted as '+15555555555'
-]
+const households = {
+  wallaceStreet: {
+    numbers: [
+      process.env.alex,
+      process.env.julia,
+      process.env.pat,
+      process.env.peter,
+    ],
+    reminders: {
+      firstAndThirdMonday: {
+        condition: function() {
+          const now = new Date();
+          const mondays = getNthDays(now, 1);
+
+          return now.getDate() === mondays[0].getDate() || now.getDate() === mondays[2].getDate();
+        },
+        message: 'Street Sweeping tonight! Park on the OTHER side.'
+      },
+      secondAndFourthTuesday: {
+        condition: function() {
+          const now = new Date();
+          const tuesdays = getNthDays(now, 2);
+
+          return now.getDate() === tuesdays[1].getDate() || now.getDate() === tuesdays[3].getDate()
+        },
+        message: 'Street Sweeping tonight! Park on OUR side.'
+      }
+    }
+  },
+  harvardStreet: {
+    numbers: [
+      process.env.zach,
+      process.env.pete,
+      process.env.aristide
+    ],
+    reminders: {
+      firstMonday: {
+        condition: function() {
+          const now = new Date();
+          const mondays = getNthDays(now, 1);
+
+          return now.getDate() === mondays[0].getDate()
+        },
+        message: 'Street Sweeping tonight! Park on the OTHER side.'
+      },
+      firstTuesday: {
+        condition: function() {
+          const now = new Date();
+          const tuesdays = getNthDays(now, 2);
+
+          return now.getDate() === tuesdays[0].getDate()
+        },
+        message: 'Street Sweeping tonight! Park on OUR side.'
+      }
+    }
+  }
+}
 
 function getNthDays(date, dayOfWeek) {
   let days = [];
@@ -27,32 +81,33 @@ function getNthDays(date, dayOfWeek) {
   return days;
 }
 
-function sendReminders(message) {
+function sendReminders(message, numbers) {
   if (message.length < 100) {
-    phoneNumbers.forEach((number) => {
+    numbers.forEach((number) => {
       client.sendMessage({
         to: number,
         from: '+15082834493',
         body: message
-      })
+      });
       console.log(`Sent to "${number}"`);
     })
     console.log(`Message: "${message}"`);
   }
 }
 
-function checkAndSend() {
-  const now = new Date();
-  const mondays = getNthDays(now, 1);
-  const tuesdays = getNthDays(now, 2);
-
-  if (now.getDate() === mondays[0].getDate() || now.getDate() === mondays[2].getDate()) {
-    sendReminders('Street Sweeping tonight! Park on the OTHER side.');
-  } else if (now.getDate() === tuesdays[1].getDate() || now.getDate() === tuesdays[3].getDate()) {
-    sendReminders('Street Sweeping tonight! Park on OUR side.');
-  } else {
-    console.log('Wrong day, no message sent.');
-  }
-}
+// Needs refactor to allow for multiple households
+// function checkAndSendHarvard() {
+//   const now = new Date();
+//   const mondays = getNthDays(now, 1);
+//   const tuesdays = getNthDays(now, 2);
+//
+//   if (now.getDate() === mondays[0].getDate()) {
+//     sendMessage('Street Sweeping tonight! Park on OUR side.', households.harvardStreetNumbers)
+//   } else if(now.getDate() === tuesdays[0].getDate()) {
+//     sendMessage('Street Sweeping tonight! Park on the OTHER side.', households.harvardStreetNumbers)
+//   else {
+//     console.log('Wrong day, no message sent.');
+//   };
+// }
 
 checkAndSend();
